1f5867679f6343a3e59d85df0a9f45f3ff0fe473
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2021-10-16 13:23:45 -0400

    Added main menu and Find the I module

---
+++ FindTheI.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace C_Sharp_Practice
+{
+    class FindTheI
+    {
+        public void Start()
+        {
+            while (true) //GameLoop
+            {
+                //Intro and difficulty selection
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                string Dline = DecorLines.Decor1;
+                string WelcomeMessage = "Find The I:";
+                string Instructions = "First question: How hard do you want this game to be? (1 or greater)";
+                
+                Console.WriteLine($"{Dline}\n{WelcomeMessage}\n{Instructions}\n{Dline}\n");
+
+                int Difficulty = 0;
+                while (true) //makes sure Difficulty is valid.
+                {
+                    Console.Write("Difficulty level: ");
+                    string str = Console.ReadLine();
+                    if (int.TryParse(str, out Difficulty) && Difficulty >= 1)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("That didn't work, try again.");
+                }
+
+                //Game round
+                Console.Clear(); 
+
+                //Draw the Ls and Is
+                int x = new Random().Next(0, Difficulty);
+                int y = new Random().Next(0, Difficulty);
+                for (int i = 0; i < Difficulty; i++)
+                {
+                    for (int j = 0; j < Difficulty; j++)
+                    {
+                        if (i == y && j == x)
+                        {
+                            Console.Write("I");
+                        }
+                        else
+                        {
+                            Console.Write("l");
+                        }
+                    }
+                    Console.Write("\n");
+                }
+
+                //validate input from player.
+                Instructions = "In the grid below, find the I and tell me where it is. \n" +
+                    "Answer in this format \"X , Y\"\n";
+                
+                Console.WriteLine($"{Dline}\n{Instructions}\n{Dline}");
+                int[] Guess = new int[2];
+
+                while (true)
+                {
+                    Console.Write("Location of I: ");
+                    string[] Input = Console.ReadLine().Split(',');
+                    if (Input.Length == 2 && 
+                        int.TryParse(Input[0], out Guess[0]) && 
+                        int.TryParse(Input[1], out Guess[1]))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("That didn't work, try again.");
+                }
+
+                if (Guess[0] == x + 1 && Guess[1] == y + 1)
+                {
+                    //Win
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("You win!");
+                }
+                else
+                {
+                    //Lose
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("You lose!");
+                }
+
+                //ask about replay
+                Console.WriteLine("playAgain?");
+                Console.Write("Y/N: ");
+                string YN = Console.ReadLine();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                if (YN != "Y" &&
+                    YN != "y") break;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,59 @@ namespace C_Sharp_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            while (true)//Main Loop
+            {
+                string WelcomeSpeech = "  Hello, thanks for checking out this collection of small works.";
+                string Instructions = "To get started, type in which module you'd like to check out!";
+
+                string[] ModulesNames = new string[] {
+                    "1. Game: Find the I.",
+                    "2. (palceholder)",
+                    "3. (palceholder)",
+                    "e. Exit program"
+                };
+
+                Console.WriteLine($"{DecorLines.Decor1}\n{WelcomeSpeech}\n{Instructions}");
+                Console.WriteLine(DecorLines.Decor1);
+                foreach (string Module in ModulesNames) Console.WriteLine(Module);
+                Console.WriteLine(DecorLines.Decor1);
+
+                Console.Write("\nModule: ");
+                string Input = Console.ReadLine();
+
+                //Compare Input to modules and start module selected
+                if (Input == "e") break;
+                if (Input == "1") new FindTheI().Start();
+                if (Input == "2") break;
+                if (Input == "3") break;
+            }
+
+            //Safe exit, save data(if needed), say goodbyes.
+            Console.WriteLine("Thanks for checking this out.\nI hope you have a great rest of your day!");
+            Console.Write("\nPress Enter to exit.");
+            Console.ReadLine();
+            Environment.Exit(1);
         }
+    }
+
+    /// <summary>
+    /// Reference/Library for decorative Lines
+    /// </summary>
+    static class DecorLines
+    {
+        /// <summary>~ ~ ~ </summary>
+        public static readonly string Decor1 = "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ";
+        
+        /// <summary>+ + + </summary>
+        public static readonly string Decor2 = "+ + + + + + + + + + + + ";
+
+        /// <summary>! ! ! </summary>
+        public static readonly string Alarm1 = "! ! ! ! ! ! ! ! ! ! ! ! ";
+        
+        /// <summary>!&!&!&</summary>
+        public static readonly string Alarm2 = "!&!&!&!&!&!&!&!&!&!&!&!&";
     }
 }
